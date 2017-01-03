b16c1e8657a2b4dec0ee55afdf7b50bd3ed61957
asemle <asemle@umass.edu>
2017-01-03 10:55:25 -0700

    beautified files

--- html.js
+++ html.js
@@ -1,114 +1,116 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
+    var flagger = true;
+    var score = 0;
+    var answerArray = [];
+    var userInput = [];
+    var flag = false;
+    var colors = ['red', 'green', 'blue', 'yellow'];
+    var count = 0;
+    var level = 3;
+    var redSound = new Audio("media/redSound.mp3")
+    var blueSound = new Audio("media/blueSound.mp3");
+    var greenSound = new Audio("media/greenSound.mp3");
+    var yellowSound = new Audio("media/yellowSound.mp3");
+    var failSound = new Audio("media/failSound.mp3");
+    var $box = $('.box')
+        //  yellowSound.play();
 
-  var flagger = true;
-     var score = 0;
-     var answerArray = [];
-     var userInput = [];
-     var flag = false;
-     var colors = ['red', 'green', 'blue', 'yellow'];
-     var count = 0;
-     var level = 3;
-     var redSound = new Audio("media/redSound.mp3")
-     var blueSound = new Audio("media/blueSound.mp3");
-     var greenSound = new Audio("media/greenSound.mp3");
-     var yellowSound = new Audio("media/yellowSound.mp3");
-     var failSound = new Audio("media/failSound.mp3");
-    //  yellowSound.play();
+    $('.log-btn').on('click', function() {
+        console.log('answers ' + answerArray);
+        console.log('User answers ' + userInput);
+    });
+    $('.start-btn').on('click', function() {
+        gameOn();
+    });
 
-     $('.log-btn').on('click',function() {
-       console.log('answers '+ answerArray);
-       console.log('User answers '+ userInput);
-     });
-     $('.start-btn').on('click', function() {
-       gameOn();
-     });
+    function gameOn() {
+        count = 0;
 
-     function gameOn() {
-       count = 0;
 
-       var $box = $('.box')
 
-       $box.removeClass('flash');
-       var interval = setInterval(function() {
-          var randoNum = Math.floor(Math.random()*(4)+0);
-          var randomColor = colors[randoNum];
-          answerArray.push(randomColor);
-           var selected = $('.' + randomColor);
-           selected.addClass(randomColor + 'Light');
+        $box.removeClass('flash');
+        var interval = setInterval(function() {
+            var randoNum = Math.floor(Math.random() * (4) + 0);
+            var randomColor = colors[randoNum];
+            answerArray.push(randomColor);
+            var selected = $('.' + randomColor);
+            selected.addClass(randomColor + 'Light');
 
-           var soundToPlay = eval(randomColor + "Sound");
-           soundToPlay.play();
-           setTimeout(function() {
-             console.log(count,flag);
-               selected.removeClass(randomColor + 'Light');
-           }, 500);
-           //end interval sets flag equal true for inputs
-           if(count >= level){
-               flag = true;
-               clearInterval(interval);
-          }
-           count++;
-       }, 1000);
-     }
+            var soundToPlay = eval(randomColor + "Sound");
+            soundToPlay.play();
+            setTimeout(function() {
+                console.log(count, flag);
+                selected.removeClass(randomColor + 'Light');
+            }, 500);
+            //end interval sets flag equal true for inputs
+            if (count >= level) {
+                flag = true;
+                clearInterval(interval);
+            }
+            count++;
+        }, 1000);
+    }
 
-     // if(flag) {
-       $box.click(function(){
-           if(flag){
-             inputs($(this));
-           }
-       });
-//2 inputs() triggers, get class name, check with answerArray, continue with game.
-     function reset() {
+    // if(flag) {
+    $box.click(function() {
+        if (flag) {
+            inputs($(this));
+        }
+    });
+    //2 inputs() triggers, get class name, check with answerArray, continue with game.
+    function reset() {
 
-       count = 0;
-       flag = false;
-       userInput.splice(0,userInput.length);
-       answerArray.splice(0,answerArray.length);
-     }
-     function inputs(that) {
+        count = 0;
+        flag = false;
+        userInput.splice(0, userInput.length);
+        answerArray.splice(0, answerArray.length);
+    }
+
+    function inputs(that) {
         var clicked = $(that).attr('class').split(" ")[1];
         console.log(clicked)
 
-       $(that).addClass(clicked +'Light');
-       var clickedSound = eval(clicked + "Sound")
+        $(that).addClass(clicked + 'Light');
+        var clickedSound = eval(clicked + "Sound")
         clickedSound.play()
-         setTimeout(function() {
-             $(that).removeClass(clicked +'Light');
-         }, 200);
+        setTimeout(function() {
+            $(that).removeClass(clicked + 'Light');
+        }, 200);
 
         var userID = that['0'].className;
         var answer = answerArray.shift();
-        var user = function(){
-          if(userID.includes('red')) return 'red';
-          else if(userID.includes('green')) return 'green';
-          else if(userID.includes('blue')) return 'blue';
-          else return 'yellow';// yellow
+        var user = function() {
+            if (userID.includes('red')) return 'red';
+            else if (userID.includes('green')) return 'green';
+            else if (userID.includes('blue')) return 'blue';
+            else return 'yellow'; // yellow
         }();
 
-        if (user!==answer) {
-          $(that).addClass('flash');
-          failSound.play();
-          console.log("RESET");
-          reset();
-          //flash red
-          //reset button
+        if (user !== answer) {
+            $(that).addClass('flash');
+            failSound.play();
+            console.log("RESET");
+            reset();
+            //flash red
+            //reset button
         }
-         count--;
-         if (count === 0) {
-           win();
-         }
-         function win() {
-           level+=1;
-           gameOn();
-           //increment number of flashes by one
-           console.log("WINNER",level);
-           score+=level;
-           console.log(score);
-           $('#score').html(score);
-           //initiate button
-         }
-     }
+        count--;
+        if (count === 0) {
+            win();
+        }
+
+        function win() {
+            level += 1;
+            gameOn();
+            //increment number of flashes by one
+            console.log("WINNER", level);
+            score += level;
+            console.log(score);
+            $('#score').html(score);
+            //initiate button
+        }
+    }
 });
 
 
@@ -128,24 +130,6 @@ $(document).ready(function(){
 //     Enable user interaction with the board, and register any clicks on the Simon tiles
 //     While the player has not entered an incorrect response, and the number of clicks is less than the length of the sequence, wait for player input
 // Continue adding rounds until the player loses
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
